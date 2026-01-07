175aa7796e0ba2e91d35548a033622c425c216ec
ksenkadinozavr-design <ksenkadinozavr@gmail.com>
2026-01-07 15:10:15 +0300

    Servers 1.0

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/ksenkadinozavr-design/todo"
 	"github.com/ksenkadinozavr-design/todo/pkg/handler"
+	"github.com/ksenkadinozavr-design/todo/pkg/repository"
+	"github.com/ksenkadinozavr-design/todo/pkg/service"
 	"log"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo_list.Server)
-	handlers := new(handler.Handler)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
