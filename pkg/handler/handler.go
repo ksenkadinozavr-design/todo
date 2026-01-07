3989fb47426ee83259b0363536c3124f9a7d3a76
ksenkadinozavr-design <ksenkadinozavr@gmail.com>
2026-01-07 14:28:28 +0300

    j

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,37 @@
 package handler
 
+import "github.com/gin-gonic/gin"
+
 type Handler struct {
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
-	
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up")
+		auth.POST("/sign-in")
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/")
+			lists.GET("/")
+			lists.GET("/:id")
+			lists.GET("/:id")
+			lists.DELETE("/:id")
+
+			items := lists.Group("/items")
+			{
+				items.POST("/")
+				items.GET("/")
+				items.GET("/:item_id")
+				items.GET("/:item_id")
+				items.DELETE("/:item_id")
+			}
+		}
+	}
+	return router
 }
--- server.go
+++ server.go
@@ -10,9 +10,10 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string) error {
+func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:              ":" + port,
+		Handler:           handler,
 		MaxHeaderBytes:    1 << 20,
 		ReadHeaderTimeout: 10 * time.Second,
 		WriteTimeout:      10 * time.Second,
@@ -20,6 +21,6 @@ func (s *Server) Run(port string) error {
 	return s.httpServer.ListenAndServe()
 
 }
-func (s *Server) Shutdown(ctx context.Context) {
+func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 }
