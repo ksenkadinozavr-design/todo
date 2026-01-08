90ac3ccd25c75812bcbdf4c4fc069784a4893a04
ksenkadinozavr-design <ksenkadinozavr@gmail.com>
2026-01-08 10:38:29 +0300

    Servers 1.0

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,25 @@ import (
 	"github.com/ksenkadinozavr-design/todo/pkg/handler"
 	"github.com/ksenkadinozavr-design/todo/pkg/repository"
 	"github.com/ksenkadinozavr-design/todo/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo_list.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
